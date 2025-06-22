35d7dadd7102c97b80badf77c9e70609d4ecf747
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-06-22 14:48:32 +0300

    Feature:Setting up my db,setting up app,.config

---
+++ server/config.py
@@ -0,0 +1,4 @@
+class Config:
+    FLASK_SQLALCHEMY_DATABASE_URI="postgresql://lateshow:1234@localhost:5432/lateshowdb"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "super-secret"
