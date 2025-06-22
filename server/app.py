9077e0bd68f98630eda35cfd780383ce5edd8ae5
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-06-22 15:10:36 +0300

    Feature:Running my initial migration ensuring my db works fine and the app is set and working

--- server/app.py
+++ server/app.py
@@ -5,18 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from .config import Config
 
-# load_dotenv()
+load_dotenv()
 db =SQLAlchemy()
 migrate =Migrate()
+
 jwt =JWTManager()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.config.from_prefixed_env()  
 
     db.init_app(app)
     migrate.init_app(app,db)
     jwt.init_app(app)
+
+    @app.route('/')
+    def home():
+        return{"message":"App is running"}
 
     return app
--- server/config.py
+++ server/config.py
@@ -1,3 +1,4 @@
+import os
 class Config:
     FLASK_SQLALCHEMY_DATABASE_URI="postgresql://lateshow:1234@localhost:5432/lateshowdb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
